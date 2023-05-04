114e4eabd09afbfeefc5613e3da1c9d2a5529e53
Arkar48 <yokesoe20678@gmal.com>
2023-05-04 17:05:50 +0630

    first commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
+import 'mdb-vue-ui-kit/css/mdb.min.css';
+
+import './fonts/fontawesome-free-6.1.2-web/css/all.min.css'
 createApp(App).use(router).mount('#app')
+
