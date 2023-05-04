afb79b13137a4bc4d41173e16aec0eff3cc98ed1
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-04 17:06:13 +0630

    add route for api call

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,34 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// testing api
+Route::middleware('auth:api')->get('/', function () {
+    return "hi";
+});
+
+Route::prefix('admin')->namespace('App\Http\Controllers')->group(function () {
+
+    // category route
+    Route::resource('category', CategoryController::class);
+
+    // job type route
+    Route::resource('jobType', JobTypeController::class);
+
+    // job post route
+    Route::resource('jobPost',JobPostController::class);
+
+});
+
+Route::prefix('frontend')->name('frontend.')->group(function(){
+
+    // category
+    Route::prefix('category')->group(function () {
+        Route::get('/',[CategoryController::class,'getAll'])->name('category.getAll');
+    });
 });
