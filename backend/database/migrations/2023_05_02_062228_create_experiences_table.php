145d63e6565f28ee4016f95119f848f91fb8d1f5
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-04 17:09:29 +0630

    migration for db

---
+++ backend/database/migrations/2023_05_02_062228_create_experiences_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('personal_info_id');
+            $table->foreign('personal_info_id')
+                ->references('id')
+                ->on('personal_infos')
+                ->onDelete('cascade');
+
+            $table->string('position')->nullable();
+            $table->string('company')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
