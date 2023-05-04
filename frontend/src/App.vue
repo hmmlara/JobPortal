e3afbdb90ed8ce17484c2731da54d6c9e824bba9
Arkar48 <yokesoe20678@gmal.com>
2023-05-04 17:05:18 +0630

    first commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  background-image: linear-gradient(to right, #c1e8ce4e 20%, #c1e8ce72 50%, #c1e8ceb8);
 }
+
 
 nav {
   padding: 30px;
@@ -24,7 +26,7 @@ nav a {
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
