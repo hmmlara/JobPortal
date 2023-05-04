97fde1c88314072d7562e6abe3647cd3badf3b29
Arkar48 <yokesoe20678@gmal.com>
2023-05-04 17:07:12 +0630

    first commit

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
+    <NavComponent/>
     <h1>This is an about page</h1>
+    <FooterComponent/>
   </div>
 </template>
+<script>
+import NavComponent from "../components/Partials/NavComponent.vue";
+import FooterComponent from "../components/Partials/FooterComponent.vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavComponent,FooterComponent
+  }
+}
+</script>
