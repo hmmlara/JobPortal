0a2bdf7d03cdf097abdbc2bb1ec15cf7d8a49bb0
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 15:10:39 +0630

    add jobpost view for router

---
+++ backend/resources/js/views/jobpost/AddJobPostView.vue
@@ -0,0 +1,19 @@
+<template>
+    <AddJobTypeComponent/>
+</template>
+
+<script>
+
+import AddJobTypeComponent from '../../components/jobtype/AddJobTypeComponent.vue';
+
+export default {
+    name: 'AddCompanyView',
+    components: {
+        AddJobTypeComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ backend/resources/js/views/jobpost/EditJobPostView.vue
@@ -0,0 +1,19 @@
+<template>
+    <EditJobPostComponent/>
+</template>
+
+<script>
+
+import EditJobPostComponent from '../../components/jobpost/EditJobPostComponent.vue';
+
+export default {
+    name: 'EditCompanyView',
+    components: {
+        EditJobPostComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
