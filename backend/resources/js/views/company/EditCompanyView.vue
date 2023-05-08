15ed61c2d402c7916a878df350c61295c1866739
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 15:09:54 +0630

    add view file for route

---
+++ backend/resources/js/views/company/EditCompanyView.vue
@@ -0,0 +1,19 @@
+<template>
+    <EditCompanyComponent/>
+</template>
+
+<script>
+
+import EditCompanyComponent from '../../components/company/EditCompanyComponent.vue';
+
+export default {
+    name: 'EditCompanyView',
+    components: {
+        EditCompanyComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
