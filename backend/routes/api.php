6a622b08e0dca93b808b3cb1fc42cbef46ee4742
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 15:04:43 +0630

    add new api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-// use App\Http\Controllers\Api\CategoryController;
 
 use App\Http\Controllers\Api\CategoryApiController;
+use App\Http\Controllers\Api\CompanyApiController;
+use App\Http\Controllers\Api\JobTypeApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -37,8 +38,11 @@ Route::prefix('admin')->namespace('App\Http\Controllers')->group(function () {
     // job post route
     Route::resource('jobPost',JobPostController::class);
 
-    // company router
+    // company route
     Route::resource('company', CompanyController::class);
+
+    // location route
+    Route::resource('location',LocationController::class);
 
 });
 
@@ -47,5 +51,12 @@ Route::prefix('frontend')->name('frontend.')->group(function(){
     // category
     Route::prefix('category')->group(function () {
         Route::get('/',[CategoryApiController::class,'getAll'])->name('category.getAll');
+    });
+    Route::prefix('company')->group(function () {
+        Route::get('/',[CompanyApiController::class,'getAll'])->name('company.getAll');
+    });
+
+    Route::prefix('jobType')->group(function(){
+        Route::get('/',[JobTypeApiController::class,'getAll'])->name('jobType.getAll');
     });
 });
