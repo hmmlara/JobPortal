2dd24a6956ef71d0fa6d1d7c2e198d26f4d94df0
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 15:13:22 +0630

    add view file for router

---
+++ backend/resources/js/views/location/AddLocationView.vue
@@ -0,0 +1,19 @@
+<template>
+    <AddLocationComponent/>
+</template>
+
+<script>
+
+import AddLocationComponent from '../../components/location/AddLocationComponent.vue';
+
+export default {
+    name: 'AddCompanyView',
+    components: {
+        AddLocationComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
