0dafbf4d40717e27158513130b5c883de423faaa
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 15:12:57 +0630

    add view file for router

---
+++ backend/resources/js/views/jobtype/JobTypeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <JobTypesComponent/>
+</template>
+
+<script>
+
+import JobTypesComponent from '../../components/jobtype/JobTypesComponent.vue';
+
+export default {
+    name: 'CompanyView',
+    components: {
+        JobTypesComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
