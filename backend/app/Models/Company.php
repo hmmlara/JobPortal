2c9990b3a8433b7f9dd4b56d91e2d5abd67a0c96
Pyae Phyo Zan <pyae418283@gmail.com>
2023-05-08 14:58:16 +0630

    add eloquent for ORM

--- backend/app/Models/Company.php
+++ backend/app/Models/Company.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'company_logo',
+    ];
+
+    public function jobPosts(){
+        return $this->hasMany(JobPost::class);
+    }
 }
