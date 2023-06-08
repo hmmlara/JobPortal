7ac7b92287b9a8a4d1437bf55368624ae9e7eb7c
Pyae Phyo Zan <pyae418283@gmail.com>
2023-06-08 12:36:45 +0630

    add otp field

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->enum('role',['admin','user'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('otp')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
