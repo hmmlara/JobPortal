9461c9635a57ba838dc892ccb8430c8a928b980b
Pyae Phyo Zan <pyae418283@gmail.com>
2023-06-26 22:04:24 +0630

    add function for save job

--- backend/app/Http/Controllers/SaveJobController.php
+++ backend/app/Http/Controllers/SaveJobController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SaveJob;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SaveJobController extends Controller
 {
     //
+
+    public function getSaveJob($userId){
+
+        $saveJob = SaveJob::with('jobPost')->where('user_id',$userId)->paginate(5);
+
+        if(empty($saveJob)){
+            return response()->json([
+                'status' => 404,
+                'statusText' => 'fail',
+                'message' => 'No save jobs',
+            ],404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'statusText' => 'success',
+            'save_jobs' => $saveJob,
+        ],200);
+    }
+
+    public function saveJob(Request $request){
+
+        $saveJob = SaveJob::create($request->all());
+
+        if($saveJob){
+            return response()->json([
+                'status' => 201,
+                'statusText' => 'success',
+                'message' => 'successfully saved',
+            ],201);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'fail to save'
+        ],400);
+    }
+
+    public function removeJob($saveJobId){
+
+        $saveJob = SaveJob::where('id',$saveJobId)->delete();
+
+        if($saveJob){
+            return response()->json([
+                'status' => 200,
+                'statusText' => 'success',
+                'message' => 'successfully removed',
+            ],201);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'fail to remove'
+        ],400);
+    }
 }
