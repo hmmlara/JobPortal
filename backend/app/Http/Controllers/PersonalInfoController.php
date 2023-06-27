827477480d9fe1e075e7093fc24ba2e1d2cd42c9
Pyae Phyo Zan <pyae418283@gmail.com>
2023-06-27 21:05:09 +0630

    add method for personal info

--- backend/app/Http/Controllers/PersonalInfoController.php
+++ backend/app/Http/Controllers/PersonalInfoController.php
@@ -2,84 +2,261 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Education;
+use App\Models\Experience;
 use App\Models\PersonalInfo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use PDOException;
 
 class PersonalInfoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    function getPersonalInfo($userId){
+
+        $personalInfo = PersonalInfo::with('educations','experiences')->where('user_id',$userId)->first();
+
+        if($personalInfo){
+            return response()->json([
+                'status' => 200,
+                'statusText' => 'success',
+                'personalInfo' => $personalInfo
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 404,
+            'statusText' => 'faild',
+            'message' => "Personal Information with user id - $userId is not founded."
+        ],404);
+    }
+    function addPersonalInfo(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'name' => 'required',
+            'email' => 'required | unique:personal_infos,email',
+            'nrc' => 'required | unique',
+            'dob' => 'required',
+            'phone' => "required",
+            'address' => "required",
+            'city' => 'required',
+            'skill' => 'required',
+            'martial_status' => 'required',
+            'gender' => 'required',
+            'education' => 'required',
+            'experience' => 'required',
+            'profile_pic' => 'required | max:2048',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'statusText' => 'fail',
+                'message' => $validator->errors(),
+            ],400);
+        }
+
+        $data = $request->all();
+
+        // change photo to 64 base characters
+        $path = $request->file('profile_pic')->getRealPath();
+        $logo = file_get_contents($path);
+        $base64 = base64_encode($logo);
+
+        $data['profile_pic'] = $base64;
+
+        $personalInfo = PersonalInfo::create($data);
+        if($personalInfo){
+            $ed_success = false;
+            $exp_success = false;
+
+            if(!empty($data['education'])){
+                try{
+                    foreach($data['education'] as $education)
+                        $education['personal_info_id'] = $personalInfo->id;
+                        Education::create($education);
+
+                    $ed_success = true;
+                }
+                catch(PDOException $e){
+                    $ed_success = false;
+                }
+
+            }
+
+            if(!empty($data['exprience'])){
+                try{
+                    foreach($data['experience'] as $experience)
+                        $experience['personal_info_id'] = $personalInfo->id;
+                        Experience::create($experience);
+
+                    $exp_success = true;
+                }
+                catch(PDOException $e){
+                    $exp_success = false;
+                }
+            }
+
+            if($ed_success && $exp_success){
+                return response()->json([
+                    'status' => 201,
+                    'statusText' => 'success',
+                    'message' => "Successfully Added Personal Information"
+                ],201);
+            }
+
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'Fail to add personal information'
+        ],200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    function editPersonalInfo(Request $request,$userId){
+
+        $updatePersonalInfo = PersonalInfo::where('user_id',$userId)->update($request->all());
+
+        if($updatePersonalInfo){
+            return response()->json([
+                'status' => 200,
+                'statusText' => "success",
+                'message' => "Successfully Updated"
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'Fail to update personal informartion'
+        ],400);
+    }
+    function editEducation(Request $request,$educationId){
+
+        $education = Education::where('id',$educationId)->update($request->all());
+
+        if($education){
+            return response()->json([
+                'status' => 200,
+                'statusText' => "success",
+                'message' => "Successfully Updated"
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'Fail to update education'
+        ],400);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+
+    function editExperience(Request $request,$experienceId){
+        $experience = Experience::where('id',$experienceId)->update($request->all());
+
+        if($experience){
+            return response()->json([
+                'status' => 200,
+                'statusText' => "success",
+                'message' => "Successfully Updated"
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => 'fail',
+            'message' => 'Fail to update experience'
+        ],400);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\PersonalInfo  $personalInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(PersonalInfo $personalInfo)
-    {
-        //
+    function addNewEducation(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'personal_info_id' => 'required',
+            'degree_name' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'statusText' => 'fail',
+                'message' => $validator->errors(),
+            ],400);
+        }
+
+        Education::create($request->all());
+
+        return response()->json([
+            'status' => 201,
+            'statusText' => 'success',
+            'message' => "Successfully Added Education"
+        ],201);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\PersonalInfo  $personalInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(PersonalInfo $personalInfo)
-    {
-        //
+
+    function addNewExperience(Request $request){
+        $validator = Validator::make($request->all(),[
+            'personal_info_id' => 'required',
+            'company_name' => 'required',
+            'positin' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'statusText' => 'fail',
+                'message' => $validator->errors(),
+            ],400);
+        }
+
+        Experience::create($request->all());
+
+        return response()->json([
+            'status' => 201,
+            'statusText' => 'success',
+            'message' => "Successfully Added Education"
+        ],201);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\PersonalInfo  $personalInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, PersonalInfo $personalInfo)
-    {
-        //
+    function deleteEducation($educationId){
+
+        $deleteEducation = Education::where('id',$educationId)->delete();
+
+        if($deleteEducation){
+            return response()->json([
+                'status' => 200,
+                'statusText' => "success",
+                'message' => "Successfully deleted"
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => "fail",
+            'message' => "FAil to  delete education"
+        ],400);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\PersonalInfo  $personalInfo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(PersonalInfo $personalInfo)
-    {
-        //
+    function deleteExperience($experienceId){
+        $deleteExperience = Experience::where('id',$experienceId)->delete();
+
+        if($deleteExperience){
+            return response()->json([
+                'status' => 200,
+                'statusText' => "success",
+                'message' => "Successfully deleted"
+            ],200);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'statusText' => "fail",
+            'message' => "FAil to  delete experience"
+        ],400);
     }
 }
