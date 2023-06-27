0dc8aef40d345d6385b1cdc5eb1e2277829a7b6f
Pyae Phyo Zan <pyae418283@gmail.com>
2023-06-27 21:06:00 +0630

    add fillabel variable

--- backend/app/Models/Experience.php
+++ backend/app/Models/Experience.php
@@ -9,6 +9,7 @@ class Experience extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['personal_info_id','position','company','start_date','end_date'];
     public function user(){
         return $this->belongsTo(PersonalInfo::class);
     }
